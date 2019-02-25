e0d312d620651f30b3bc9e346d13c7b419601052
David Thai <davidthai@dafuloth.me>
2019-02-25 15:41:00 +0000

    feature test for viewing bookmarks at the /bookmarks route

---
+++ spec/features/viewing_bookmarks_spec.rb
@@ -0,0 +1,15 @@
+feature 'Viewing bookmarks' do
+  scenario 'visiting the index page' do
+    visit('/')
+    expect(page).to have_content "Bookmark Manager"
+  end
+
+  scenario 'bookmarks displayed' do
+    visit '/bookmarks'
+
+    expect(page).to have_content "http://www.makers.tech"
+    expect(page).to have_content "http://www.github.com"
+    expect(page).to have_content "http://www.youtube.com"
+  end
+
+end
