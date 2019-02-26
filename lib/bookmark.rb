27a6b779d28f47af6823463925384c81b5fcaeef
David Thai <davidthai@dafuloth.me>
2019-02-26 16:22:57 +0000

    removed commented out, obsolete lines

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -7,16 +7,6 @@ class Bookmark
     db = PG.connect(dbname:'bookmark_manager')
     bookmarks_data = db.exec('SELECT * FROM bookmarks;')
 
-    bookmarks_data.map { |bookmark| bookmark["url"] }
-  
-    # bookmarks = Array.new
-    # bookmarks_data.each { |bookmark| bookmarks << bookmark["url"] }
-
-    # bookmarks
-    # [
-    #   "http://www.makers.tech",
-    #   "http://www.github.com",
-    #   "http://www.youtube.com"
-    # ]
+    bookmarks_data.map { |bookmark| bookmark['url'] }
   end
 end
--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,14 +3,6 @@ require 'pg'
 
 describe Bookmark do
   describe '.all' do
-    # xit 'returns all Bookmarks' do
-    #   bookmarks = Bookmark.all
-      
-    #   expect(bookmarks).to include("http://www.makersacademy.com")
-    #   expect(bookmarks).to include("http://www.google.com")
-    #   expect(bookmarks).to include("http://www.destroyallsoftware.com")
-    # end
-
     it 'connects to database called "bookmark_manager"' do
       expect(PG.connect(dbname:'bookmark_manager')).to be_instance_of(PG::Connection)
     end
