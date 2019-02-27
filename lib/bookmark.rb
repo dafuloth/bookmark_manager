96ee8bcfebb5dde4f2072d08dc82a9da1b6844de
David Thai <davidthai@dafuloth.me>
2019-02-27 14:48:40 +0000

    changed Bookmark to access bookmarks from db

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -5,6 +5,7 @@ class Bookmark
     db = PG.connect(dbname: 'bookmark_manager')
     bookmarks_data = db.exec('SELECT * FROM bookmarks;')
 
+    # Use map so that an array is returned
     bookmarks_data.map { |bookmark| bookmark['url'] }
   end
 end
